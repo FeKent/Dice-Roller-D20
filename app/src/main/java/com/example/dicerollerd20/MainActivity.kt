8103a97dc8b233893275a730f75780aa0b584d54
SnippyRex <kentf022@gmail.com>
2023-05-13 15:24:38 +0100

    Reused Dice Roller App code to use a D20 rather than D6

--- app/src/main/java/com/example/dicerollerd20/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerd20/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.dicerollerd20
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dicerollerd20.ui.theme.DiceRollerD20Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +23,56 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DiceRollerD20Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+                DiceRollerAppD20()
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    DiceRollerD20Theme {
-        Greeting("Android")
+fun DiceRollerAppD20() {
+    DiceWithButtonAndImage(modifier = Modifier
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center))
+}
+
+@Composable
+fun DiceWithButtonAndImage (modifier: Modifier = Modifier){
+    var result by remember { mutableStateOf(1) }
+    val imageResource = when (result){
+        1 -> R.drawable._1
+        2 -> R.drawable._2
+        3 -> R.drawable._3
+        4 -> R.drawable._4
+        5 -> R.drawable._5
+        6 -> R.drawable._6
+        7 -> R.drawable._7
+        8 -> R.drawable._8
+        9 -> R.drawable._9
+        10 -> R.drawable._10
+        11 -> R.drawable._11
+        12 -> R.drawable._12
+        13 -> R.drawable._13
+        14 -> R.drawable._14
+        15 -> R.drawable._15
+        16 -> R.drawable._16
+        17 -> R.drawable._17
+        18 -> R.drawable._18
+        19 -> R.drawable._19
+        else -> R.drawable._20
     }
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = imageResource),
+            contentDescription = result.toString()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {result = (1..20).random()}) {
+            Text(stringResource(R.string.roll))
+        }
+    }
+}
 }
